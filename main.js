282af561a8005c2157420e92393ccf5abfeca755
HermioneParedesHTP <paredes.hr@obmontessori.edu.ph>
2026-01-20 13:43:04 +0800

    Add divide function to calculate quotient

--- main.js
+++ main.js
@@ -11,3 +11,10 @@ function add() {
     var add = Number(num1) + Number (num2);
     window.alert(add);
 }
+
+function divide() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var quotient = Number(num1) ** Number (num2);
+    window.alert(quotient);
+}
