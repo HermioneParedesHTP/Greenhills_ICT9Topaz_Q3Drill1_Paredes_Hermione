539ae1b02a6c19efdd16aece87b962bc76edde43
HermioneParedesHTP <paredes.hr@obmontessori.edu.ph>
2026-01-20 13:25:29 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function subtract() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var diff = Number(num1) - Number (num2);
+    window.alert(diff);
+}
