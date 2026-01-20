55eaf316cf986d92b668f7a80fa556a26a58d030
HermioneParedesHTP <paredes.hr@obmontessori.edu.ph>
2026-01-20 13:42:00 +0800

    Add function to calculate the sum of two numbers

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function subtract() {
     var diff = Number(num1) - Number (num2);
     window.alert(diff);
 }
+
+function add() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var add = Number(num1) + Number (num2);
+    window.alert(add);
+}
