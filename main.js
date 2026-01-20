cb44527c43e72845d34dabf18139fc122222a446
HermioneParedesHTP <paredes.hr@obmontessori.edu.ph>
2026-01-20 13:44:58 +0800

    Refactor arithmetic functions for multiplication and division

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-function subtract() {
+function multiply() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
-    var diff = Number(num1) - Number (num2);
-    window.alert(diff);
+    var product = Number(num1) * Number (num2);
+    window.alert(product);
 }
-
+function divide() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var quotient = Number(num1) ** Number (num2);
+    window.alert(quotient);
+}
 function add() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
     var add = Number(num1) + Number (num2);
     window.alert(add);
 }
-
-function divide() {
+function subtract() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
-    var quotient = Number(num1) ** Number (num2);
-    window.alert(quotient);
+    var diff = Number(num1) - Number (num2);
+    window.alert(diff);
 }
